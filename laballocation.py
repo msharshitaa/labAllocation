c92ed783dce476612beef2f186041adc130cf68e
Harshitaa M S <88045719+msharshitaa@users.noreply.github.com>
2023-07-10 09:59:05 +0530

    Add files via upload

---
+++ laballocation.py
@@ -0,0 +1,15 @@
+x = int(input())
+y = int(input())
+z = int(input())
+number = int(input())
+while True:
+    number += 1
+    if number == x:
+        print("L1")
+        break
+    if number == y:
+        print("L2")
+        break
+    if number == z:
+        print("L3")
+        break
